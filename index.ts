4200f245999f3b4d542869f0d66df391c8eaa98f
Yasir Erkam <yasir.erkam17@gmail.com>
2023-12-10 21:23:27 +0300

    newly

--- index.ts
+++ index.ts
@@ -7,14 +7,17 @@ export default class ProxyOPI {
         await ProxyProvider.asyncInit(options, pathProxyList, timeout);
     }
 
-    static async getProxyList() {
-        return await ProxyProvider.getProxyList();
+    static async getProxyList(newly: boolean = false) {
+        if (newly)
+            return await ProxyProvider.getNewProxyList();
+        else
+            return await ProxyProvider.getProxyList();
     }
 }
 
 async function test() {
     await ProxyOPI.asyncInit({ protocols: ['https'], anonymityLevels: ['elite', 'anonymous'] }, "./proxyList.json", 4 * 60 * 60 * 1000);
-    const proxyList = await ProxyOPI.getProxyList();
+    const proxyList = await ProxyOPI.getProxyList(true);
     // console.log("\nProxy list is:\n", proxyList);
 }
 
