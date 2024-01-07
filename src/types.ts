bcf828392d2791e2deb25a1f2bb11d6fd9ad0efc
Yasir Erkam <yasir.erkam17@gmail.com>
2024-01-07 23:53:51 +0300

    types

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 
-const enum Protocol { http = "http", https = "https", socks4 = "socks4", socks5 = "socks5", unknown = "unknown" }
+enum Protocol { http = "http", https = "https", socks4 = "socks4", socks5 = "socks5", unknown = "unknown" };
 
-const enum AnonymityLevel { transparent = "transparent", anonymous = "anonymous", elite = "elite", unknown = "unknown" }
+enum AnonymityLevel { transparent = "transparent", anonymous = "anonymous", elite = "elite", unknown = "unknown" };
 
 type Proxy = { ip: string, port: string, protocols: Protocol[], sourceSite: string, anonymityLevel?: AnonymityLevel, lastTested?: string, country?: string, city?: string, isp?: string, speed?: string, uptime?: string, responseTime?: string, verified?: string, };
 
