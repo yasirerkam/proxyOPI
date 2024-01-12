a6c49cb0cbd49040f4abf5b25910ec574ddf7df2
Yasir Erkam <yasir.erkam17@gmail.com>
2024-01-12 22:07:26 +0300

    test

---
+++ __tests__/index.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from '@jest/globals';
+import { ProxyOPI, Proxy, ProxyList, Protocol } from "../src/index.ts";
+
+describe('sum module', () => {
+    test('testtt', () => {
+
+
+        // expect(xyz(a, b)).toBe(3);
+    });
+});
