de777b25b178688cfd9509a60298c2a610fb318d
Yasir Erkam <yasir.erkam17@gmail.com>
2024-10-15 17:28:46 +0300

    jsonfile

--- src/jsonFileOps.ts
+++ src/jsonFileOps.ts
@@ -1,11 +1,19 @@
 import fs from "node:fs";
-import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import PathLb from "path";
 
 export default class JsonFileOps {
 
-    static isFileExists(path: string): boolean {
+    static readonly __filename = fileURLToPath(import.meta.url);
+    static readonly __dirname = dirname(__filename);
+
+
+    static isFileExists(pathObj: string): boolean {
+        pathObj = PathLb.join(__dirname, pathObj);
+
         try {
-            fs.accessSync(path, fs.constants.F_OK);
+            fs.accessSync(pathObj, fs.constants.F_OK);
             return true;
         } catch (err) {
             return false;
@@ -13,21 +21,24 @@ export default class JsonFileOps {
     }
 
     static readJson(pathObj: string) {
+        pathObj = PathLb.join(__dirname, pathObj);
+
         let obj: any;
-        const pathParsed = path.parse(pathObj);
+        const pathParsed = PathLb.parse(pathObj);
 
         try {
-            console.log("\n'%s' is reading...", pathParsed.base);
+            console.log(`\n'${pathParsed.base}' is reading...`,);
             const dictData = fs.readFileSync(pathObj, 'utf-8');
             obj = JSON.parse(dictData);
-            console.log("'%s' has been read.", pathParsed.base);
+            console.log(`'${pathParsed.base}' has been read.`,);
         } catch (error: any) {
             if (error.code === 'ENOENT') {
-                console.log("'%s' is not found. Create it first!", pathParsed.base);
+                console.warn(`'${pathObj}' is not found. Create it first!`,);
+                console.warn(`Current directory: '${process.cwd()}'`,);
                 return undefined;
             }
 
-            console.log("An error occurred while reading '%s':\n", pathParsed.base, error);
+            console.error(`An error occurred while reading '${pathParsed.base}':\n${error}`);
             return undefined;
         }
 
@@ -35,19 +46,21 @@ export default class JsonFileOps {
     }
 
     static writeJson(obj: any, pathObj: string, options: any, format = false) {
-        const pathParsed = path.parse(pathObj);
+        pathObj = PathLb.join(__dirname, pathObj);
+
+        const pathParsed = PathLb.parse(pathObj);
 
         try {
-            console.log("\n'%s' is writing...", pathParsed.base);
+            console.log(`\n'${pathParsed.base}' is writing...`);
             let dictData;
             if (format)
                 dictData = JSON.stringify(obj, null, 4);
             else
                 dictData = JSON.stringify(obj);
             fs.writeFileSync(pathObj, dictData, options);
-            console.log("Writing to file is complete '%s'", pathParsed.base);
+            console.log(`'${pathParsed.base}' is written.`);
         } catch (error) {
-            console.log("An error occurred while writing '%s':\n", pathParsed.base, error);
+            console.error(`An error occurred while writing '${pathParsed.base}':\n${error}`);
             return;
         }
     }
